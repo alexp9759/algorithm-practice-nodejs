f50cbed7fe3899a5bd5c204de6f7e67095b19319
alexp <alexp9759@gmail.com>
2022-03-08 10:14:37 -0600

    added can-construct problem and brute-force solution Next step optimize solution for speed

--- can-construct.js
+++ can-construct.js
@@ -1,3 +1,37 @@
 /*
-
+    write canConstruct(target, wordBank), takes in target string and array of strings as args
+    return a bool for if target can be constructed by concatenating elements of wordBank array
+    wordBank elements can be reused
  */
+
+const canConstruct = (target, wordBank) => {
+    if (target === '') {
+        return true;
+    }
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            if (canConstruct(suffix, wordBank) === true) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
+
+
+
+console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])) //true
+console.log(canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); //false
+console.log(canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o','t'])); //true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    'e',
+    'ee',
+    'eee',
+    'eeee',
+    'eeeee',
+    'eeeeee'
+])); //false
+
