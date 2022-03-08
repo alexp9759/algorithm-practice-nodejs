4e8244cab7069815c01c438a316d4acdc2f19c55
alexp <alexp9759@gmail.com>
2022-03-08 14:03:03 -0600

    added tabulation solution to fib.js

--- fib.js
+++ fib.js
@@ -21,11 +21,30 @@ const fib = (n) => {
 * 0(n) space
 */
 
+/*
 const fib = (n, memo = {}) => {
     if (n in memo) return memo[n];
     if (n <= 2) return 1;
     memo[n] = fib(n-1, memo) + fib(n-2, memo);
     return memo[n];
+};
+*/
+
+/*
+* ********************************************************************
+* tabulated solution
+* O(n) time
+* 0(n) space
+*/
+
+const fib = (n) => {
+    const table = Array(n + 1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i <= n; i++) {
+        table[i + 1] += table[i];
+        table[i + 2] += table[i];
+    }
+    return table[n];
 };
 
 
@@ -34,8 +53,8 @@ const fib = (n, memo = {}) => {
 * tests
 */
 
-console.log(fib(5));
 console.log(fib(6));
+console.log(fib(7));
 console.log(fib(8));
 console.log(fib(50));
 
