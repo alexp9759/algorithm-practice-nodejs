0ba0a656c310cbf334e57abbb9905dc311493b74
alexp <alexp9759@gmail.com>
2022-03-08 14:34:17 -0600

    added tabulation solution to grid-traveler

--- grid-traveler.js
+++ grid-traveler.js
@@ -30,6 +30,7 @@ const gridTraveler = (m, n) => {
 *
 */
 
+/*
 const gridTraveler = (m, n, memo={}) => {
     const key = m + ',' + n;
     // are the args in the memo
@@ -40,6 +41,29 @@ const gridTraveler = (m, n, memo={}) => {
     memo[key] = gridTraveler(m-1, n, memo) + gridTraveler(m, n-1, memo);
     return memo[key];
 };
+*/
+
+/*
+* ********************************************************************
+* tabulated solution
+* O(m*n) time
+* 0(m*n) space
+*/
+
+const gridTraveler = (m, n) => {
+    const table = Array(m + 1)
+        .fill()
+        .map(() => Array(n + 1).fill(0));
+    table[1][1] = 1;
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            const current = table[i][j];
+            if (j + 1 <= n) table[i][j + 1] += current;
+            if (i + 1 <= m) table[i + 1][j] += current;
+        }
+    }
+    return table[m][n];
+}
 
 /*
 * ********************************************************************
