5449bfe4466109eb4343101b579f46d11656129b
alexp <alexp9759@gmail.com>
2022-03-08 10:25:14 -0600

    added memoized solution to can-construct problem

--- can-construct.js
+++ can-construct.js
@@ -3,7 +3,7 @@
     return a bool for if target can be constructed by concatenating elements of wordBank array
     wordBank elements can be reused
  */
-
+/*
 const canConstruct = (target, wordBank) => {
     if (target === '') {
         return true;
@@ -21,6 +21,46 @@ const canConstruct = (target, wordBank) => {
     return false;
 };
 
+//time O(n^m * m)
+//space O(m^2)
+
+console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])) //true
+console.log(canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); //false
+console.log(canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o','t'])); //true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    'e',
+    'ee',
+    'eee',
+    'eeee',
+    'eeeee',
+    'eeeeee'
+])); //false
+*/
+
+//memoize ************************
+
+const canConstruct = (target, wordBank, memo={}) => {
+    if (target in memo) return memo[target]
+    if (target === '') {
+        return true;
+    }
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            if (canConstruct(suffix, wordBank, memo) === true) {
+                memo[target] = true;
+                return true;
+            }
+        }
+    }
+
+    memo[target] = false;
+    return false;
+};
+
+//time O(n*m^2)
+//space O(m^2)
 
 
 console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])) //true
@@ -34,4 +74,3 @@ console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
     'eeeee',
     'eeeeee'
 ])); //false
-
