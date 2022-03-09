281ad648604a4a49978053367e4308a74704e127
alexp <alexp9759@gmail.com>
2022-03-08 21:29:05 -0600

    added tabulation solution to count-construct problem

--- count-construct.js
+++ count-construct.js
@@ -39,6 +39,7 @@ const countConstruct = (target, wordBank) => {
 *
 */
 
+/*
 const countConstruct = (target, wordBank, memo={}) => {
     if (target in memo) return memo[target];
     if (target === '') return 1;
@@ -55,7 +56,36 @@ const countConstruct = (target, wordBank, memo={}) => {
     memo[target] = totalCount;
     return totalCount;
 };
+*/
 
+/*
+* =========================================================================
+* tabulated
+*
+* m=targetSum
+* n=numbers.length
+*
+* O(m^2 * n) time
+* O(m) space
+*
+ */
+
+const countConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+
+    for (let i = 0; i <= target.length; i++) {
+        if (table[i] > 0) {
+            for (let word of wordBank) {
+                if (target.slice(i, i + word.length) === word) {
+                    table[i + word.length] += table[i];
+                }
+            }
+        }
+    }
+
+    return table[target.length];
+}
 
 /*
 * ********************************************************************
